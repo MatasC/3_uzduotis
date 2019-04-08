4562c9891f934dbf3bfbe5b9093801fb41cc5deb
MatasC <matas.cenys@gmail.com>
2019-04-08 13:33:28 +0300

    Pataisyta Time.h

--- Headers/Time.h
+++ Headers/Time.h
@@ -7,8 +7,7 @@
 class Time
 {
 private:
-	std::chrono::time_point<std::chrono::high_resolution_clock> pradzia_;
-	std::chrono::time_point<std::chrono::high_resolution_clock> pabaiga_;
+	std::chrono::time_point<std::chrono::high_resolution_clock> pradzia_,pabaiga_;
 	std::chrono::duration<double> skirtumas_;
 public:
 	void Start_clock();
